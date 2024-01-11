c1c80c6e0cf88554d17e331724df94dc16637f1d
bohdan-markiv <bohdan.markiv18@gmail.com>
2024-01-11 11:53:43 +0100

    -started cleaning the data

---
+++ data_research.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# Create the data frames
+erasmus_db = pd.read_csv("data/Erasmus.csv", sep=";")
+mutation_log_db = pd.read_csv("data/Mutatielog_id.csv", sep=";")
+toelichting_db = pd.read_excel("data/Toelichting.xlsx")
+
+# Create new columns for date and time
+mutation_log_db['DatumRegistratie'] = pd.to_datetime(
+    mutation_log_db['DatumRegistratie'])
+
+mutation_log_db['date_column'] = mutation_log_db['DatumRegistratie'].dt.date
+mutation_log_db['time_column'] = mutation_log_db['DatumRegistratie'].dt.time
+mutation_log_db.drop('DatumRegistratie', axis=1, inplace=True)
+
+# Fix float values.
+for col in erasmus_db.columns:
+    try:
+        erasmus_db[col] = erasmus_db[col].str.replace(
+            ',', '.').astype(float)
+    except Exception:
+        continue
