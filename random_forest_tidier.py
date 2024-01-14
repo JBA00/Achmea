40de49ff4b4c77e9ea7b418231b0d263792a87cf
bohdan-markiv <bohdan.markiv18@gmail.com>
2024-01-14 12:39:36 +0100

    -added trained model and some graphs

--- random_forest_tidier.py
+++ random_forest_tidier.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import make_scorer, precision_score, recall_score, f1_score, accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
@@ -14,6 +13,7 @@ from imblearn.under_sampling import RandomUnderSampler
 import pandas as pd
 from data_research import create_df
 import matplotlib.pyplot as plt
+import joblib
 
 
 random_state = 1810
@@ -38,19 +38,25 @@ numeric_features = X.select_dtypes(
 categorical_features = X.select_dtypes(
     include=['object', 'string']).columns.tolist()
 
+boolean_features = X.select_dtypes(
+    include=['bool']).columns.tolist()
+
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', Pipeline(steps=[('imputer', SimpleImputer(
             strategy='mean')), ('scaler', StandardScaler())]), numeric_features),
         ('cat', Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')), (
             'onehot', OneHotEncoder(drop='first'))]), categorical_features),
-    ]
+        # Placeholder step for boolean features
+        ('bool', Pipeline(
+            steps=[('placeholder', 'passthrough')]), boolean_features),
+    ],
+    remainder='passthrough'  # Include the rest of the columns as they are
 )
-
 
 param_grid = {
     # Tune max_features from 1 to 200 for the RandomForestClassifier
-    'classifier__max_features': list(range(1, 400, 6)),
+    'classifier__max_features': list(range(1, 100, 4)),
 }
 
 scoring = {
@@ -63,7 +69,7 @@ scoring = {
 cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=random_state)
 
 rf_classifier = RandomForestClassifier(
-    n_estimators=200, random_state=random_state)
+    n_estimators=400, random_state=random_state)
 
 model_pipeline = ImbPipeline([
     ('preprocessor', preprocessor),
@@ -83,15 +89,21 @@ print("Best Hyperparameters:", grid_search.best_params_)
 
 # Make predictions on the test set using the best model
 best_model = grid_search.best_estimator_
+joblib.dump(best_model, 'best_model.joblib')
 predictions = best_model.predict(X_test)
 
-predictions.value_
 # Evaluate the model performance on the test set
 accuracy = accuracy_score(y_test, predictions)
 print(f"Accuracy: {accuracy}")
 
 report = classification_report(y_test, predictions)
 print("Classification Report:\n", report)
+
+# Predictions including G category
+
+predictions_full = best_model.predict(erasmus_db.drop("status", axis=1))
+erasmus_db["predictions"] = predictions_full
+# Create the plots
 
 results = grid_search.cv_results_
 max_features_values = param_grid['classifier__max_features']
@@ -121,4 +133,36 @@ plt.ylabel('Score')
 plt.legend()
 plt.grid(True)
 
+plt.show()
+
+# Importance of variables
+# Fit the best model on the entire training data
+best_model.fit(X, y)
+
+# Access feature importances
+feature_importances = best_model.named_steps['classifier'].feature_importances_
+
+# Get feature names from the preprocessor
+numeric_features_preprocessed = best_model.named_steps['preprocessor'].transformers_[
+    0][2]
+
+
+boolean_features_preprocessed = best_model.named_steps['preprocessor'].transformers_[
+    2][2]
+
+all_features = numeric_features + boolean_features_preprocessed
+# Create a DataFrame for better visualization
+feature_importance_df = pd.DataFrame(
+    {'Feature': all_features, 'Importance': feature_importances})
+
+# Sort by importance in descending order
+feature_importance_df = feature_importance_df.sort_values(
+    by='Importance', ascending=False)
+
+# Plot the top 10 features
+plt.figure(figsize=(10, 6))
+plt.barh(feature_importance_df['Feature'][:10],
+         feature_importance_df['Importance'][:10])
+plt.xlabel('Feature Importance')
+plt.title('Top 10 Feature Importances')
 plt.show()
